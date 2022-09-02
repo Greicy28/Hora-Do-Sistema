56920024b07e3029c80e6487b8ec20812d5df1ff
Greicy28 <111903522+Greicy28@users.noreply.github.com>
2022-09-02 17:27:01 -0300

    Hora do Sistema

---
+++ src/HoraDoSistema.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class HoraDoSistema {
+public static void main(String[]args) {
+
+Date relogio = new Date();
+System.out.println("A Hora Do Sistema é");
+System.out.println(relogio.toString());
+}
+}
